c2f7054cd551a8dee1ac7538bc943585a76e7111
Eddú Meléndez <eddu.melendez@gmail.com>
2023-06-23 17:18:38 -0600

    Polish

--- src/test/java/com/example/testcontainersbuildpacksexamples/BuildpackTest.java
+++ src/test/java/com/example/testcontainersbuildpacksexamples/BuildpackTest.java
@@ -3,11 +3,17 @@ package com.example.testcontainersbuildpacksexamples;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.PullImageResultCallback;
 import dev.snowdrop.buildpack.Buildpack;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
@@ -18,14 +24,46 @@ public class BuildpackTest {
 
     private static DockerClient dockerClient = DockerClientFactory.lazyClient();
 
-    @Test
-    void test() throws InterruptedException {
+    private static Path buildpacks;
+
+    private static final File cwd = getRootProject();
+
+    private static File finalFile;
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
         pullImage();
 
-        File cwd = getRootProject();
+        buildpacks = Files.createTempDirectory("buildpacks");
 
+        Files.walk(cwd.toPath())
+                .filter(path -> (path.toFile().isDirectory() && path.toFile().getPath().equals("./src/main")) || (path.toFile().isFile() && path.toFile().getName().equals("pom.xml")))
+                .forEach(path -> {
+                    try {
+                        Path targetPath = buildpacks.resolve(cwd.toPath().relativize(path));
+                        if (path.toFile().isDirectory()) {
+                            FileUtils.copyDirectory(path.toFile(), targetPath.toFile());
+                        }
+                        else {
+                            FileUtils.copyFile(path.toFile(), targetPath.toFile());
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+        finalFile = new File(buildpacks.toFile(), ".");
+    }
+
+    @AfterAll
+    static void afterAll() throws IOException {
+        FileUtils.deleteDirectory(buildpacks.toFile());
+    }
+
+    @Test
+    void test()  {
         Buildpack.builder().withDockerClient(dockerClient)
-                .addNewFileContent(new File(cwd, "."))
+                .addNewFileContent(finalFile)
                 .withFinalImage("test-spring-app")
                 .withEnvironment(Map.of("BP_JVM_VERSION", "17.*"))
                 .withLogLevel("info")
@@ -40,13 +78,9 @@ public class BuildpackTest {
     }
 
     @Test
-    void testNativeImage() throws InterruptedException {
-        pullImage();
-
-        File cwd = getRootProject();
-
+    void testNativeImage() {
         Buildpack.builder().withDockerClient(dockerClient)
-                .addNewFileContent(new File(cwd, "."))
+                .addNewFileContent(finalFile)
                 .withBuilderImage("paketobuildpacks/builder:tiny")
                 .withFinalImage("test-native-spring-app")
                 .withEnvironment(Map.ofEntries(entry("BP_JVM_VERSION", "17.*"),
@@ -74,7 +108,7 @@ public class BuildpackTest {
     }
 
     private static void pullImage() throws InterruptedException {
-        dockerClient.pullImageCmd("tianon/true").exec(new PullImageResultCallback()).awaitCompletion();
+        dockerClient.pullImageCmd("tianon/true:latest").exec(new PullImageResultCallback()).awaitCompletion();
     }
 
 }
